e6332adf2cfae16bcf275380199adedb8a0c77e6
Bartek Stokłosa <b_stoklosa@o2.pl>
2020-03-27 09:27:53 +0100

    login, register done

--- charityapp/forms.py
+++ charityapp/forms.py
@@ -4,13 +4,12 @@ from django import forms
 from charityapp.models import Category, Institution, Donation
 
 
-class RegisterForm(ModelForm, Form):
-    password = forms.CharField(widget=forms.PasswordInput())
-    password_confirm = forms.CharField(widget=forms.PasswordInput())
-
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'email', 'password']
+class RegisterForm(forms.Form):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Imię'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Nazwisko'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Hasło'}))
+    password_confirm = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Powtórz hasło'}))
 
 
     def clean(self):
@@ -19,16 +18,20 @@ class RegisterForm(ModelForm, Form):
         password_confirm = cleaned_data.get("password_confirm")
         email = cleaned_data.get("email")
         if password != password_confirm:
-            raise forms.ValidationError("Hasło niepoprawne!")
+            raise forms.ValidationError("Hasła nie zgadzają się!")
         try:
             user = User.objects.get(email=email)
-            if user.email ==email:
-                raise forms.ValidationError("Podany email już istneiej")
+            if user.email == email:
+                raise forms.ValidationError("Podany email już istnieje w bazie danych")
         except User.DoesNotExist:
             return None
-
+        return cleaned_data
 
 class LoginForm(ModelForm):
     class Meta:
         model = User
         fields = ['email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(attrs={'placeholder': 'Hasło'}),
+            'email': forms.TextInput(attrs={'placeholder': 'Email'})
+        }
--- charityapp/views.py
+++ charityapp/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
 
-from charityapp.forms import RegisterForm
+from charityapp.forms import RegisterForm, LoginForm
 from charityapp.models import Donation, Institution
 
 
@@ -47,12 +49,24 @@ class AddDonationView(View):
 
 class LoginView(View):
     def get(self, request):
-        return render(request, "forms/login.html")
+        form = LoginForm()
+        return render(request, "forms/login.html", {"form": form})
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("landing-page")
+            else:
+                return render(request, "forms/login.html", {"form": form})
 
 
 class RegisterView(View):
     def get(self, request):
-        form = RegisterForm
+        form = RegisterForm()
         return render(request, "forms/register.html", {"form": form})
     def post(self, request):
         form = RegisterForm(request.POST)
@@ -71,5 +85,4 @@ class RegisterView(View):
             return redirect("login")
         else:
             return render(request, "forms/register.html", {"form": form})
-
 
