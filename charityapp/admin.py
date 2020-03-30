ac63de905e17c07165fb78f5dd45ac6ae6fd506d
Bartek Stokłosa <b_stoklosa@o2.pl>
2020-03-30 14:50:43 +0200

    js form work step 3

--- charityapp/admin.py
+++ charityapp/admin.py
@@ -3,5 +3,5 @@ from charityapp.models import *
 # Register your models here.
 
 admin.site.register(Category)
-admin.site.register(Institution)
+admin.site.register(Institution, InstitutionAdmin)
 admin.site.register(Donation, DonationAdmin)
